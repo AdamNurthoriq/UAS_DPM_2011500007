e7b9343f095e809ceaa44a9117649fe152aa3aaf
AdamNurthoriq <adamnur288@gmail.com>
2022-06-21 20:09:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uasdpm2011500007/Dosen.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm2011500007
+
+class Dosen (
+    var nidn: String,
+    var nmDosen: String,
+    var Jabatan: String,
+    var golongan: String,
+    var pendidikan: String,
+    var Keahlian: String,
+    var program: String
+)
